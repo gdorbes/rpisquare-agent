2e7d4dacecfdf6d270a6c3a1b25003505ebd0221
Guillaume DORBES <guillaume@dorbes.com>
2025-08-13 11:29:07 +0200

    refactor

--- esm/hwi.mjs
+++ esm/hwi.mjs
@@ -8,11 +8,12 @@ import {log} from "./dev.mjs"
 // -------------------------------------------------------------------
 // CONSTANTS AND VARIABLES
 // -------------------------------------------------------------------
-const PLATFORM = os.platform()
-const CMDS = {
+const MY_COMMANDS = {
     serial: {
         linux: {
+            // Command to execute
             cmd: "cat /proc/cpuinfo | grep Serial",
+            // Filter applied to result
             flt: str => {
                 return str.split(":")[1].trim()
             }
@@ -54,6 +55,7 @@ const CMDS = {
     }
 }
 
+export let hw = {}
 // -------------------------------------------------------------------
 //FUNCTIONS
 /** ------------------------------------------------------------------
@@ -90,50 +92,118 @@ const run = function (cmd) {
 
 /** ------------------------------------------------------------------
  * @function runCmd
- * @description Exec command defined in CMDS
+ * @description Exec command defined in MY_COMMANDS
  * @param {String} name - Command name
  * @return {Promise}
  */
 const runCmd = async name => {
-    if (CMDS[name][PLATFORM]) {
-        const result = await run(CMDS[name][PLATFORM].cmd)
-        if (result.type === "stdout") {
-            return CMDS[name][PLATFORM].flt(result.data)
+    const platform = os.platform()
+    if (MY_COMMANDS[name][platform]) {
+        const returned = await run(MY_COMMANDS[name][platform].cmd)
+        if (returned.type === "stdout") {
+            return MY_COMMANDS[name][platform].flt(returned.data)
         } else {
-            log("serial cmd error:", result.type, result.data)
+            log("serial cmd error:", returned.type, returned.data)
             return "undefined"
         }
     }
 
-    log("unsupported platform:", PLATFORM)
+    log("unsupported platform:", platform)
     return "undefined"
 }
 
 /** ------------------------------------------------------------------
- * @function serial
+ * @function hw.serial
  * @description Return hardware serial number
  * @return {Promise}
  */
-export const serial = async () => {
+hw.serial = async () => {
     return await runCmd("serial")
 }
 /** ------------------------------------------------------------------
- * @function model
- * @description Return hardware model
+ * @function hw.model
+ * @description Return hardware model and store if for further call
  * @return {Promise}
  */
-export const model = async () => {
-    return await runCmd("model")
+hw.model = async () => {
+
+    if (!hw.model.value) {
+        hw.model.value = await runCmd("model")
+        hw.model.value = hw.model.value.toLowerCase()
+    }
+    return hw.model.value
 }
 
 /** ------------------------------------------------------------------
- * @function iface
+ * @function hw.iface
  * @description Return network interface
  * @return {Promise}
  */
-export const iface = async () => {
+hw.iface = async () => {
     return await runCmd("iface")
 }
+/** ------------------------------------------------------------------
+ * @function hw.name
+ * @description Return hardware name
+ * @return {Promise}
+ */
+hw.name = async () => {
+    let model = await hw.model()
+    model = model.toLowerCase()
+    if (model.search("raspberry") !== -1) {
+        return "raspberry"
+    }
+    if (model.search("mac") !== -1) {
+        return "mac"
+    }
+    return "unknown"
+}
+/** ------------------------------------------------------------------
+ * @function osInfo
+ * @description Return OS version and codename
+ * @return {Promise}
+ */
+hw.os = async () => {
+    const hwName = await hw.name()
+    const response = {
+        version: "unknown",
+        name: "unknown"
+    }
+    let returned
+    if (hwName === "mac") {
+        const latestVersions = {
+            "15": "sequoia",
+            "14": "sonoma",
+            "13": "ventura",
+            "12": "monterey",
+            "11": "big sur",
+        }
+        returned = await run("sw_vers -productVersion")
+        if (returned.type === "stdout") {
+            response.version = returned.data.replace("\n", "").trim()
+            const known = latestVersions[response.version.substring(0, 2)]
+            known ? response.name = known : false
+        }
+    }
+    if (hwName === "raspberry") {
+        returned = await run("cat /etc/os-release")
+        if (returned.type === "stdout") {
+
+            const lines = returned.data.split("\n")
+            const info = {}
+            lines.forEach(line => {
+                const [key, value] = line.split("=")
+                if (key && value) {
+                    info[key] = value.replace(/"/g, "");
+                }
+            })
+            response.version = info.VERSION_ID
+            response.name = info.VERSION_CODENAME.toLowerCase()
+        }
+    }
+    return response
+}
+
 
 // -------------------------------------------------------------------
 // EoF
